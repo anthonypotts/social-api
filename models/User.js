d11026b3ed1790a0a63d72ce6ec90bc5a2c515ec
Anthony Potts <anthonypotts@Anthonys-MacBook-Pro-2.local>
2021-03-18 22:16:11 -0500

    initial commit with folders and files

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true
+        }
+    }
+)
