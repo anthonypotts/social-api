250dde2ebcae64d5f16a9d2ecf43efe00b705128
Anthony Potts <anthonypotts@Anthonys-MacBook-Pro-2.local>
2021-03-19 00:04:28 -0500

    saving spot, adding routes/controllers/models

--- models/User.js
+++ models/User.js
@@ -1,12 +1,45 @@
 const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
 
 const UserSchema = new Schema(
     {
         username: {
             type: String,
+            unique: true,
             required: true,
             trim: true
-        }
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/.+@.+\..+/, 'Must match a valid email address!']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
     }
-)
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+// create the User model with UserSchema
+const User = model('User', UserSchema);
+
+module.exports = User;
